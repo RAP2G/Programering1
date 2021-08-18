5ee7dab60a72722f3c540476c99e699cb195933a
Soma Szabó <soma.szabo@elev.ga.ntig.se>
2021-08-18 09:29:01 +0200

    initial commit

---
+++ Day 1/helloworld.py
@@ -0,0 +1,7 @@
+# Soma Szabo
+# INF20
+# 08-06-2021
+# Hello World
+
+
+print("Hello World")
---
+++ Day 1/wellcome.py
@@ -0,0 +1,10 @@
+# Soma Szabo
+# INF20
+# 08-06-2021
+# Wellcome
+
+print("Welcome to this program!")
+print("What's your name")
+name = input("Your name: ")
+print("Nice to see you", name)
+""
