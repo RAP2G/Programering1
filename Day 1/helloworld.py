52c82ff889544eb9bca78bca4252d9ec9c633c75
Soma Szabó <soma.szabo@elev.ga.ntig.se>
2021-08-31 13:45:08 +0200

    Cats

--- Day 1/helloworld.py
+++ Day 1/helloworld.py
@@ -1,7 +1,8 @@
 # Soma Szabo
 # INF20
-# 08-06-2021
+# 24-08-2021
 # Hello World
 
 
 print("Hello World")
+print("")
--- Day 1/wellcome.py
+++ Day 1/wellcome.py
@@ -1,9 +1,11 @@
 # Soma Szabo
 # INF20
-# 08-06-2021
-# Wellcome
+# 24-08--2021
+# Wellcome screen
 
-print("Welcome to this program!")
-print("What's your name")
-name = input("Your name: ")
+print("Welcome to the Game!")
+name = input("What's your name? ")
 print("Nice to see you", name)
+cats = input("How many cats did you see on your way here? ")
+paws = int(cats)*4
+print("You saw", cats, "cats on the way here? That is", paws, "amount of paws!")
