cf9d0bd74639179e6d8f3f6bca7569160e4b15bf
Soma Szabó <szabosonka@gmail.com>
2022-03-23 16:30:39 +0100

    Added Quiz Creation and Loading

--- Kalsser/nti.py
+++ Kalsser/nti.py
@@ -4,19 +4,15 @@
 # Teknikare
 
 
-from socket import NI_MAXHOST, NI_NUMERICHOST
-
-
 class NtiElev:
 
     def __init__(self, name, age, sex, _class):
-        self.name  = name
+        self.name = name
         self.age = age
-        self.sex=sex
+        self.sex = sex
         self._class = _class
         self.pronoun = "hon" if self.sex == "tjej" else "han"
         self.pospro = "hennes" if self.sex == "tjej" else "hans"
-
 
     def __str__(self):
         return f"NTI Eleven heter {self.name}. {self.pronoun.capitalize()} är en {self.sex} och är {self.age} år gammal. {self.pronoun.capitalize()} går i klassen {self._class}"
@@ -24,27 +20,32 @@ class NtiElev:
     def return_name(self):
         return f"Eleven heter {self.name}."
 
+
 class Teknikare(NtiElev):
 
     def __init__(self, name, age, sex, _class, favorite_game):
         super().__init__(name, age, sex, _class)
         self.favorite_game = favorite_game
-    
+
     def __str__(self):
         return super().__str__() + f" {self.pospro.capitalize()} favorit spel är {self.favorite_game}"
-    
+
     def gaming(self):
         return f"{self.name} spelar {self.favorite_game}."
+
 
 class ITare(NtiElev):
     def __init__(self, name, age, sex, _class, mess):
         super().__init__(name, age, sex, _class)
-        self.mess =mess
+        self.mess = mess
 
     def __str__(self):
         return super().__str__() + f" {self.pronoun.capitalize()} har varit stökigt {self.mess} gånger."
+
+
 def main():
-    soma = Teknikare("Soma", "17", "kille", "TEINF-20", "The Legend Of Zelda Breath Of The Wild")
+    soma = Teknikare("Soma", "17", "kille", "TEINF-20",
+                     "The Legend Of Zelda Breath Of The Wild")
     armand = ITare("Armand", "17", "kille", "IT-20", "0")
     ellen = Teknikare("Ellen", "17", "tjej", "TEINF-20", "Stardew Valley")
     max = Teknikare("Max", "17", "kille", "TEINF-20", "Read Dead Redemption 2")
@@ -57,5 +58,6 @@ def main():
         elif i == ITare:
             pass
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     main()
--- quiztime.py
+++ quiztime.py
@@ -6,14 +6,17 @@
 # Imports
 
 from random import shuffle, randint
+from os import listdir
+from os.path import isfile, join
 
 
 # Classes
 
+
 class Question:
     def __init__(self, question: str, answer: str, alts: list) -> None:
         """
-        This class takes in a name:str, an answer:str and alternative answers:list
+        This class requires three parameters: a question:str, an answer:str and alternative answers:list
 
         """
         self.question = question
@@ -31,9 +34,16 @@ class Question:
             return False
 
     def ask_question(self):
+        """
+        This method print out the question and the answers which the user can choose from.
+
+        """
         print(self.question)
         print()
         if len(self.alts) <= 3:
+            """
+            Prints out the real answer and all available alternatives if there are 3 or less alternatives
+            """
             answers = [self.answer]
             for i in self.alts:
                 answers.append(i)
@@ -45,6 +55,9 @@ class Question:
                 else:
                     print(f"        {x}", end="")
         else:
+            """
+            Prints out the answer and 3 random alternatives from a shuffled list of alternatives
+            """
             answers = [self.answer]
             shuffle(self.alts)
             for i in range(3):
@@ -61,12 +74,21 @@ class Question:
 class Quiz:
 
     def __init__(self, name: str, questions: list) -> None:
+        """
+        This class requires two parameters: a name:str and a list of question objects:list
+        """
         self.name = name
         self.questions = questions
         self.asked = []
         self.points = 0
 
     def ask_question(self):
+        """
+        Calls on the ask_question method of a random question object.
+        Then asks the user for input which is then validated using the question objects check_answer method.
+        Returns true if check_answer is true
+        Returns false otherwise
+        """
         question_number = randint(0, len(self.questions)-1)
         question = self.questions[question_number]
         self.asked.append(self.questions.pop(question_number))
@@ -79,27 +101,93 @@ class Quiz:
             return False
 
     def get_name(self):
+        """
+        Returns the name of object
+        """
         return self.name
 
     def reset(self):
+        """
+        'Resets' the quiz by appending all of the asked questions back to the question list
+        """
         self.questions.extend(self.asked)
         self.asked = []
 
     def get_quiz_length(self):
+        """
+        Returns the length of the list questions 
+
+        """
         return len(self.questions)
 
     def get_asked_length(self):
+        """
+        Returns the length of the list questions 
+
+        """
         return len(self.asked)
 
 
-def create_quiz(name, quiz_file):
+def load_quiz():
+    name = ""
     questions = []
-    with open(f"{quiz_file}") as file:
-        for ques in file.readlines():
-            params = ques.split("/")
-            questions.append(
-                Question(params[0], params[1], params[2].split(",")))
-    return Quiz(name, questions)
+    quizzes = []
+    onlyfiles = [f for f in listdir(
+        "Quiz_Folder") if isfile(join("Quiz_Folder", f))]
+    for i in onlyfiles:
+        with open(f"Quiz_Folder/{i}") as file:
+            name = file.readline().strip()
+            for ques in file.readlines():
+                params = ques.split("/")
+                questions.append(
+                    Question(params[0], params[1], params[2].strip().split(",")))
+        quizzes.append(Quiz(name, questions))
+        questions = []
+    return quizzes
+
+
+def create_quiz():
+    quizzes = []
+    while True:
+        print("Would you like to create a quiz? y/n ")
+        inp = input("Answer: ")
+        if inp.lower() == "y":
+            name = input("Name your quiz: ")
+            questions = []
+            while True:
+                question = input("Input a question: ")
+                answer = input("Input the answer to the question: ")
+                alternatives = []
+                while True:
+                    alternatives.append(input("Input an alternative answer: "))
+                    inp = input(
+                        "Would you like to add another alternative? y/n ")
+                    if inp.lower() == "y":
+                        pass
+                    else:
+                        break
+                questions.append(Question(question, answer, alternatives))
+                inp = input("Would you like to add another question? y/n ")
+                if inp.lower() == "y":
+                    pass
+                else:
+                    break
+            quizzes.append(Quiz(name, questions))
+            with open(f"Quiz_Folder/{name}.txt", "a") as f:
+                f.write(f"{name}\n")
+                for i in questions:
+                    alts = i.alts
+                    savable_alts = ''
+                    for k in alts:
+                        if k == alts[len(alts)-1]:
+                            savable_alts += k
+                        else:
+                            savable_alts += f"{k},"
+                    save_string = f"{i.question}/{i.answer}/{savable_alts}\n"
+                    f.write(save_string)
+        else:
+            return quizzes
+            break
 
 
 def game(quiz: list):
@@ -118,12 +206,14 @@ def game(quiz: list):
 
 
 def main():
+    quizzes = []
+    quizzes.extend(create_quiz())
+    quizzes.extend(load_quiz())
     while True:
-        quizzes = []
-        quizzes.append(create_quiz("Quiz1", "quiz_1.txt"))
         game(quizzes)
-        inp = input("Would you like to continue?y/n")
+        inp = input("Would you like to continue?y/n ")
         if inp.lower() == "n":
+            print("Good bye!")
             break
 
 
